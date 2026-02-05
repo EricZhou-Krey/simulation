6664d03b3c75d1ef58b1627694001df86720e296
Eric Zhou <ericzhou1101@gmail.com>
2026-02-05 15:33:54 +0000

    Final setup of structure wot wasm rust objects, ready to start graphics implmentation

--- www/index.js
+++ www/index.js
@@ -1,3 +1,8 @@
 import * as sim from "lib-simulation-wasm";
 
 const simulation = new sim.Simulation();
+const world = simulation.world();
+
+for (const animal of world.animals) {
+  console.log(animal.x, animal.y);
+}
