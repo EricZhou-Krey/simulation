0c26e96e24352607391a60b4e690ecfd02b55c42
Eric Zhou <ericzhou1101@gmail.com>
2026-02-06 20:02:37 +0000

    starting visual

--- www/index.js
+++ www/index.js
@@ -6,3 +6,12 @@ const world = simulation.world();
 for (const animal of world.animals) {
   console.log(animal.x, animal.y);
 }
+
+const viewport = document.getElementById("viewport");
+const ctxt = viewport.getContext("2d");
+
+ctxt.fillStyle = "rgb(0, 0, 0)";
+
+for (const animal of simulation.world().animals) {
+    ctxt.fillRect(animal.x * viewport.width, animal.y * viewport.height, 15, 15);
+}
