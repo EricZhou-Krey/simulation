4ea99e94500a10f98d43f4a5c77a652980319cf2
Eric Zhou <ericzhou1101@gmail.com>
2026-02-11 13:31:42 +0000

    food + colouring

--- app/simulation-wasm/src/lib.rs
+++ app/simulation-wasm/src/lib.rs
@@ -32,6 +32,9 @@ impl Simulation {
 pub struct World {
     #[wasm_bindgen(getter_with_clone)]
     pub animals: Vec<Animal>,
+
+    #[wasm_bindgen(getter_with_clone)]
+    pub foods: Vec<Food>,
 }
 
 #[wasm_bindgen]
@@ -45,8 +48,9 @@ pub struct Animal {
 impl From<&sim::World> for World {
     fn from(world: &sim::World) -> Self {
         let animals = world.animals().iter().map(Animal::from).collect();
+        let foods = world.foods().iter().map(Food::from).collect();
 
-        Self { animals }
+        Self { animals, foods }
     }
 }
 
@@ -56,6 +60,22 @@ impl From<&sim::Animal> for Animal {
             x: animal.position().x,
             y: animal.position().y,
             rotation: animal.rotation().angle(),
+        }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct Food {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<&sim::Food> for Food {
+    fn from(food: &sim::Food) -> Self {
+        Self {
+            x: food.position().x,
+            y: food.position().y,
         }
     }
 }
--- www/index.js
+++ www/index.js
@@ -8,46 +8,67 @@ ctxt.fillStyle = "rgb(0, 0, 0)";
 
 const forward_scale = 1.5;
 CanvasRenderingContext2D.prototype.drawTriangle =
-    function (x, y, size, rotation) {
-        this.beginPath();
+  function (x, y, size, rotation) {
+    this.beginPath();
 
-        this.moveTo(
-            x - Math.sin(rotation) * size * forward_scale,
-            y + Math.cos(rotation) * size * forward_scale,
-        );
+    this.moveTo(
+        x - Math.sin(rotation) * size * forward_scale,
+        y + Math.cos(rotation) * size * forward_scale,
+    );
 
-        this.lineTo(
-            x - Math.sin(rotation + 2.0 / 3.0 * Math.PI) * size,
-            y + Math.cos(rotation + 2.0 / 3.0 * Math.PI) * size,
-        );
+    this.lineTo(
+        x - Math.sin(rotation + 2.0 / 3.0 * Math.PI) * size,
+        y + Math.cos(rotation + 2.0 / 3.0 * Math.PI) * size,
+    );
 
-        this.lineTo(
-            x - Math.sin(rotation + 4.0 / 3.0 * Math.PI) * size,
-            y + Math.cos(rotation + 4.0 / 3.0 * Math.PI) * size,
-        );
+    this.lineTo(
+        x - Math.sin(rotation + 4.0 / 3.0 * Math.PI) * size,
+        y + Math.cos(rotation + 4.0 / 3.0 * Math.PI) * size,
+    );
 
-        this.lineTo(
-            x - Math.sin(rotation) * size * forward_scale,
-            y + Math.cos(rotation) * size * forward_scale,
-        );
+    this.lineTo(
+        x - Math.sin(rotation) * size * forward_scale,
+        y + Math.cos(rotation) * size * forward_scale,
+    );
 
-        this.stroke();
-    };
+    this.fill();
+  };
+
+CanvasRenderingContext2D.prototype.drawCircle = 
+  function (x, y, radius) {
+    this.beginPath();
+
+    this.arc(x, y, radius, 0, 2.0 * Math.PI);
+
+    this.fill();
+  };
 
 function redraw() {
-    ctxt.clearRect(0, 0, viewport.width, viewport.height);
+  ctxt.clearRect(0, 0, viewport.width, viewport.height);
 
-    simulation.step();
+  simulation.step();
 
-    for (const animal of simulation.world().animals) {
-        ctxt.drawTriangle(
-            animal.x * viewport.width,
-            animal.y * viewport.height,
-            0.01 * viewport.width,
-            animal.rotation,
-        );
-    }
-    requestAnimationFrame(redraw);
+  const world = simulation.world();
+  
+  for (const food of world.foods) {
+    ctxt.fillStyle = "rgb(0, 255, 128)"
+    ctxt.drawCircle(
+      food.x * viewport.width,
+      food.y * viewport.height,
+      (0.01 / 2.0) * viewport.height,
+    );
+  }
+
+  for (const animal of world.animals) {
+    ctxt.fillStyle = "rgb(255, 255, 255)"
+    ctxt.drawTriangle(
+      animal.x * viewport.width,
+      animal.y * viewport.height,
+      0.01 * viewport.width,
+      animal.rotation,
+    );
+  }
+  requestAnimationFrame(redraw);
 }
 
 redraw();
